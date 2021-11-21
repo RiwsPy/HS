4479de9df40c5809160c42f0250e283ebd5a7b08
RiwsPy <thibault.debruyne@hotmail.fr>
2021-11-21 17:15:56 +0100

    NAME added in DATABASES

--- api_battlegrounds/settings/travis.py
+++ api_battlegrounds/settings/travis.py
@@ -1,9 +1,11 @@
 from . import *
+from dotenv import load_dotenv
 
+load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
+        'NAME': os.getenv('DB_TABLE'),
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '',
