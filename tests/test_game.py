b436f91b7de281ce09dc03ded3669619c33716af
RiwsPy <thibault.debruyne@hotmail.fr>
2021-11-21 22:45:37 +0100

    using ./manage.py test instead of pytest

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,20 +1,17 @@
-import pytest
-from game import Game
 from base.entity import Entity, Card
 from base.enums import CardName, GOLD_BY_TURN, Race
-import base.db_card
-from base.sequence import Sequence
+from django.test import TestCase
 
+class TestGame(TestCase):
+    def setUp(self):
+        pass
 
-@pytest.fixture()
-def charge_init_db_card(monkeypatch):
-    # non fonctionnel
-    def charge_all_mock(*args, **kwargs):
-        return base.db_card.charge_all_cards('tests/HStat_mock.json')
-    monkeypatch.setattr(base.db_card, 'charge_all_cards', charge_all_mock)
-
-
-def test_game_type_ban(charge_init_db_card):
-    g = Card(CardName.DEFAULT_GAME, type_ban=Race('BEAST').hex)
-    assert g.type_ban == Race('BEAST').hex
-    #assert len(g.craftable_cards.exclude(synergy='BEAST')) == len(g.craftable_cards)
+    def test_game_type_ban(self):
+        g = Card(CardName.DEFAULT_GAME, type_ban=Race('BEAST').hex)
+        self.assertEqual(g.type_ban, Race('BEAST').hex)
+        """
+        self.assertEqual(
+            len(g.craftable_cards.exclude(synergy='BEAST')),
+            len(g.craftable_cards)
+        )
+        """
