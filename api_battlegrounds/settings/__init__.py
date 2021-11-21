ab938b918d59591cadef63d2a1955bfb22087663
RiwsPy <thibault.debruyne@hotmail.fr>
2021-11-21 19:48:15 +0100

    purge card migrations

--- api_battlegrounds/settings/__init__.py
+++ api_battlegrounds/settings/__init__.py
@@ -82,6 +82,8 @@ WSGI_APPLICATION = 'api_battlegrounds.wsgi.application'
 
 DATABASES = {
     'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_TABLE'),
         'USER': os.getenv('DB_USER'),
