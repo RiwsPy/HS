4f4f399b075b1451080d6d9dce9a80f5bd40d6cf
RiwsPy <thibault.debruyne@hotmail.fr>
2021-11-21 18:52:06 +0100

    change travis settings

--- api_battlegrounds/settings/travis.py
+++ api_battlegrounds/settings/travis.py
@@ -1,11 +1,9 @@
 from . import *
-from dotenv import load_dotenv
 
-load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_TABLE'),
+        'NAME': '',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'localhost',
